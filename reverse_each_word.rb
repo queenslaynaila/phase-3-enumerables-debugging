92d3f5abffb8fd256d621231a5d15f37543044ef
Queenslay Jema <queenslay.naila@student.moringaschool.com>
2022-10-19 08:13:26 +0300

    done

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,1 +1,19 @@
-# Write your code here
+ require "pry"
+#  def reverse_each_word (sentence)
+#     words = sentence.split(" ")
+#     reversed_words = []
+#     words.each do |word|
+#          reversed_words << word.reverse
+#     end
+#     reversed_words.join(" ")
+#  end
+#  puts reverse_each_word("Hello there, and how are you?")
+# binding.pry
+# 0
+def reverse_each_word (sentence)
+  words = sentence.split
+  reversed_words = words.map do |word|
+    word.reverse
+  end
+  reversed_words.join(" ")
+end
